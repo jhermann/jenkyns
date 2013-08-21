c8bace73f3f27b62fb92597d2e8787237b2a64ad
jhermann <jh@web.de>
2013-08-21 21:42:58 +0200

    fixed sys.path inserts

--- doc/conf.py
+++ doc/conf.py
@@ -12,13 +12,13 @@
 # serve to show the default.
 
 import sys, os, re
-import pavement
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, pavement.projectdir)
-sys.path.insert(0, pavement.projectdir / "src")
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
+import pavement
 
 # -- General configuration -----------------------------------------------------
 
