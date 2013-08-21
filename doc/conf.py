69a7a328ce7c29abc781db5f1df6ffae0535cb2a
jhermann <jh@web.de>
2013-08-21 22:27:05 +0200

    'view on github' added; some style changes

--- doc/conf.py
+++ doc/conf.py
@@ -100,8 +100,9 @@ html_theme = 'jenkyns'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-}
+html_theme_options = dict(
+    github_repo_url = "https://github.com/jhermann/jenkyns",
+)
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
